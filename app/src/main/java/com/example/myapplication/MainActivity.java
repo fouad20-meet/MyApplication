7edaefa136f4ada54a50f9cdf2532e68bbc8a59e
Fouad Haddad <fouad.ha2003@gmail.com>
2020-09-17 09:19:35 +0300

    123

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,46 +11,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
-    private EditText name,pass,email;
-    private Button login,clear;
-    private CheckBox check;
+public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name = findViewById(R.id.etName);
-        pass = findViewById(R.id.etPassword);
-        email = findViewById(R.id.etEmail);
-        login = findViewById(R.id.login);
-        login.setOnClickListener(this);
-        clear = findViewById(R.id.clear);
-        check = findViewById(R.id.savedetails);
-        clear.setOnLongClickListener(this);
+
     }
-    @Override
-    public void onClick(View view) {
-        if (login == view) {
-            if (name.getText().toString().equals("") && pass.getText().toString().equals("") && email.getText().toString().equals(""))
-                Toast.makeText(this,"Error! Empty Fields", Toast.LENGTH_LONG).show();
-            else {
-                if (email.getText().toString().contains("@") && email.getText().toString().contains(".com")) {
-                    Intent intent = new Intent(this, SecActivity.class);
-                    Person person = new Person(name.getText().toString(), email.getText().toString(), pass.getText().toString(), 1, check.isChecked());
-                    intent.putExtra("Person", person);
-                    startActivity(intent);
-                }
-                else
-                    Toast.makeText(this,"Not a Viable Email!",Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-    @Override
-    public boolean onLongClick(View view) {
-        if(view == clear){
-            name.setText("");
-            email.setText("");
-            pass.setText("");
-        }
-        return true;
-    }
+
+
+
 }
